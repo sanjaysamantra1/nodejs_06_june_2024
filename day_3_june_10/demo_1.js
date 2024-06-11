2b57cc260928f74408355a7d1477f3ed5054437e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-06-11 20:06:37 +0530

    day 3

---
+++ day_3_june_10/demo_1.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+
+// Older (string concatenation)
+console.log('Addition of ' + a + ' and ' + b + ' is ' + (a + b));
+
+// new (template literal)
+console.log(`Addition of ${a} and ${b} is ${a + b}`)
+
+// window.alert(`Addition of ${a} and ${b} is ${a + b}`);
+// document.write(`Addition of ${a} and ${b} is ${a + b}`)
