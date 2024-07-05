c73ed49fd1841c0da3ed39ed60974f42bc18537c
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-07-05 21:10:57 +0530

    day 22

---
+++ day_23_july_5/chalk_demo_1.mjs
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+console.log(chalk.blue.bgRed.bold('Hello world!'));
---
+++ day_23_july_5/minimist_demo_1.js
@@ -0,0 +1,1 @@
+const minimist = require('minimist');
