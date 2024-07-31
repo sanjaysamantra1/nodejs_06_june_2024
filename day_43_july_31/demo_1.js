a5d83b094538839f8c7db10f3a9b2bed184dc146
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-07-31 21:27:15 +0530

    graphql

---
+++ day_43_july_31/demo_1.js
@@ -0,0 +1,37 @@
+var express = require("express")
+var { createHandler } = require("graphql-http/lib/use/express")
+var { buildSchema } = require("graphql")
+var { ruruHTML } = require("ruru/server")
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    abcd: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    abcd() {
+        return "Hello world!"
+    },
+}
+
+var app = express()
+
+// Create and use the GraphQL handler.
+app.all(
+    "/graphql",
+    createHandler({
+        schema: schema,
+        rootValue: root,
+    })
+)
+app.get("/", (req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+
+// Start the server at port
+app.listen(4000)
+console.log("Running a GraphQL API server at http://localhost:4000/graphql")
