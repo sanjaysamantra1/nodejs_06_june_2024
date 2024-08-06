4379a020593b5da4f813668aee8db5aae0308a35
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-08-06 20:40:20 +0530

    nodemailer

---
+++ day_48_aug_6/send_mail.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+const express = require("express");
+
+const app = express();
+
+app.get('/sendMail', async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "sanjaysamantra2@gmail.com",
+            pass: "vqnzangbviohjbmu", // google app password
+        },
+    });
+
+    const mailInfo = {
+        from: "sanjaysamantra2@gmail.com",
+        to: ["jagtappooja177@gmail.com", "developerpraveen11@gmail.com",
+            "jabinnahid14@gmail.com", "vamsimyneni501@gmail.com"],
+        subject: "Test Email using NodeJS", // Subject line
+        html: `
+                <h1>This is a Test E-Mail</h1>
+                <h3 style="color:red;text-align:center">Congratulations!!!!</h3
+                <p>Please follow the below instructions to claim your amount</p>
+
+                <pre>
+                    Hellooo ooooooooooo
+                    Hiiiiiiiiiiiii
+                    Good Evening
+                </pre>
+            `
+    }
+    await transporter.sendMail(mailInfo);
+    console.log('Mail Sent Successfully!!!')
+});
+app.listen(5000);
+
