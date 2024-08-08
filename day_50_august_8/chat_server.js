50bf39859c7bcddf7d489ea9b0a47c12c48a925e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-08-08 21:33:57 +0530

    chat app

---
+++ day_50_august_8/chat_server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+let app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer(app);
+const io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('A New Client Joined');
+
+    socket.on('setName', (name) => {
+        socket.userName = name;
+    })
+    // reply chat data to client
+    socket.on('chat', (data) => {
+        let userName = socket.userName;
+        let currentTime = new Date().toLocaleTimeString();
+        let msgData = { message: data.message, userName: userName, time: currentTime };
+        io.emit('chat', msgData);
+    })
+});
+server.listen(5000, () => { console.log('server running') });
